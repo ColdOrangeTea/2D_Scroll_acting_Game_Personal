c240f92ee6799243631a25c5993676dbf89581ad
ColdOrangeTea <coldogt@gmail.com>
2023-09-24 08:52:06 +0800

    2023/9/24 刪除舊檔

--- Assets/Scripts/UnitStateMachine/State/SupState/AbilityState/DeadState.cs
+++ Assets/Scripts/UnitStateMachine/State/SupState/AbilityState/DeadState.cs
@@ -2,15 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class DeadState : MonoBehaviour
+public class DeadState : State
 {
-    [SerializeField] protected UnitStateMachineManager unitStateMachineManager;
-    [SerializeField] protected GameObject thisThing;
-
-    void Awake()
+    public DeadState(PlayerMovement playerMovement, PlayerStateMachine stateMachine, UnitAttribute unitAttribute, string animBoolName) : base(playerMovement, stateMachine, unitAttribute, animBoolName)
     {
-        InitDeadState();
+
     }
-    public abstract IEnumerator Dying();
-    public abstract void InitDeadState();
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+    public override void Exit()
+    {
+        base.Enter();
+    }
+    public override void LogicUpdate() //Update
+    {
+    }
+
+    public override void PhysicsUpdate() //FixedUpdate
+    {
+        DoChecks();
+    }
+
+    public override void AnimationTrigger()
+    {
+
+    }
+    public override void AnimationFinishTrigger() => isAnimationFinished = true;
+
 }
